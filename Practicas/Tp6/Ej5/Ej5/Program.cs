893db64c463770303f9159ca2da5c8348a6b95e0
champal18 <juampacabezon@hotmail.com>
2016-05-03 18:15:33 -0300

    Fin Tp6

---
+++ Practicas/Tp6/Ej5/Ej5/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Champal
+ * Date: 3/5/2016
+ * Time: 17:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej5
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			ClaseA a = new ClaseA();
+			a.ej();
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+	
+	class ClaseA
+	{
+		char c;
+		static string st;
+		void metodo1()
+		{
+			st="string";
+			c='A';
+		}
+
+		static void metodo2()
+		{
+			new ClaseA().c='a';
+			st="st2";
+			//c='B';
+		}
+		
+		public void ej()
+		{
+			this.metodo1();
+			ClaseA.metodo2();
+		}
+	}
+}
---
+++ Practicas/Tp6/Ej6/Ej6/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Champal
+ * Date: 3/5/2016
+ * Time: 18:09
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej6
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine(new Registro().Id);
+			Console.WriteLine(new Registro().Id);
+			Console.WriteLine(new Registro().Id);
+			Console.WriteLine(new Registro().Id);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+	
+	class Registro
+	{
+		private int idRegistro;
+		private static int nro = 0;
+		
+		public Registro()
+		{
+			Registro.nro++;
+			this.idRegistro = nro;
+		}
+		
+		public int Id
+		{
+			get
+			{
+				return this.idRegistro;
+			}
+		}
+	}
+}
