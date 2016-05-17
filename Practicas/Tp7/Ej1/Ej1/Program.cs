39d7de6231afb7e836d6746b4c53786e71f2ebce
champal18 <juampacabezon@hotmail.com>
2016-05-17 14:52:15 -0300

    TP7

---
+++ Practicas/Tp7/Ej1/Ej1/Program.cs
@@ -0,0 +1,73 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Champal
+ * Date: 9/5/2016
+ * Time: 15:41
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej1
+{
+	delegate void TrabajandoEventHandler();
+	
+	class Program
+	{
+		public static void Main()
+		{
+			Trabajador o=new Trabajador();
+			//o.Trabajando = new TrabajandoEventHandler(F);
+			
+			//Ej3
+			//o.Trabajando += new TrabajandoEventHandler(G);
+			
+			//Ej4
+			//o.Trabajando += new TrabajandoEventHandler(G);
+			//o.Trabajando += new TrabajandoEventHandler(G);
+			
+			//Ej5
+			//o.Trabajando = o.Trabajando + new TrabajandoEventHandler(G);
+			//o.Trabajando = new TrabajandoEventHandler(G);
+			
+			//Ej6
+			//o.Trabajar();
+			//Console.WriteLine("-------------------------");
+			
+			//Console.WriteLine("Todo listo para comenzar");
+			//o.Trabajando -= new TrabajandoEventHandler(F);
+			//o.Trabajando += new TrabajandoEventHandler(G);
+			//o.Trabajando-=G;
+			
+			//Ej7
+			o.Trabajando -= new TrabajandoEventHandler(F);
+			
+			o.Trabajar();
+			Console.ReadKey(true);
+		}
+	
+		private static void F()
+		{
+			Console.WriteLine("El trabajo se ha iniciado");
+		}
+		
+		private static void G()
+		{
+			Console.WriteLine("Ejecutando el metodo G");
+		}
+	}
+	
+	class Trabajador
+	{
+		public TrabajandoEventHandler Trabajando;
+		
+		public void Trabajar()
+		{
+			//Ej2
+			if(Trabajando != null)
+				Trabajando();
+			//realiza algún trabajo
+			Console.WriteLine("Trabajo concluido");
+		}
+	}
+}
---
+++ Practicas/Tp7/Ej8/Ej8/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Champal
+ * Date: 9/5/2016
+ * Time: 16:55
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Ej8
+{
+delegate void TrabajandoEventHandler();
+class Program
+{
+private static TrabajandoEventHandler delegado1,delegado2;
+public static void Main()
+{
+delegado1 += new TrabajandoEventHandler(F);
+delegado1 += new TrabajandoEventHandler(G);
+delegado2 += new TrabajandoEventHandler(F);
+delegado2 += new TrabajandoEventHandler(G);
+Trabajador o=new Trabajador();
+o.Trabajando = delegado1+delegado2;
+o.Trabajar();
+Console.ReadKey(true);
+}
+private static void F(){
+Console.WriteLine("El trabajo se ha iniciado");
+}
+private static void G(){
+Console.WriteLine("Ejecutando el método G");
+}
+}
+class Trabajador{
+public TrabajandoEventHandler Trabajando;
+public void Trabajar(){
+if (Trabajando != null) Trabajando();
+//realiza algún trabajo
+Console.WriteLine("Trabajo concluido");
+}
+}
+}
